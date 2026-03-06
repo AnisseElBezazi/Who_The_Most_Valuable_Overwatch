229b412da3558c9c373ed0b4a8a48c5e5bc71b2d
Quentin Deglas <qudeglas@gmail.com>
2026-03-06 09:54:51 +0100

    chore: remove .next from git tracking

--- .next/dev/server/app/api/player/[battletag]/route.js
+++
@@ -1,6 +0,0 @@
-var R=require("../../../../chunks/[turbopack]_runtime.js")("server/app/api/player/[battletag]/route.js")
-R.c("server/chunks/node_modules_next_46009367._.js")
-R.c("server/chunks/[root-of-the-server]__88e68409._.js")
-R.c("server/chunks/_next-internal_server_app_api_player_[battletag]_route_actions_08f8013a.js")
-R.m("[project]/node_modules/next/dist/esm/build/templates/app-route.js { INNER_APP_ROUTE => \"[project]/src/app/api/player/[battletag]/route.ts [app-route] (ecmascript)\" } [app-route] (ecmascript)")
-module.exports=R.m("[project]/node_modules/next/dist/esm/build/templates/app-route.js { INNER_APP_ROUTE => \"[project]/src/app/api/player/[battletag]/route.ts [app-route] (ecmascript)\" } [app-route] (ecmascript)").exports
